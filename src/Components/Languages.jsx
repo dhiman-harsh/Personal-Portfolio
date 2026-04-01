8df89220a0ef39fa5b214cc363b9e2be0cf1bfb2
Harsh Dhiman <harsh61355@gmail.com>
2026-04-01 18:10:15 +0530

    Updated Language section

--- src/Components/Languages.jsx
+++ src/Components/Languages.jsx
@@ -7,11 +7,11 @@ const Languages = () => {
             <SkillHeading value="Languages" />
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 flex-1">
                 <SkillBlock value="JavaScript" skill="javascript" />
+                <SkillBlock value="Python" skill="python" />
+                <SkillBlock value="Java" skill="java" />
+                <SkillBlock value="C++" skill="cplusplus" />
                 <SkillBlock value="HTML 5" skill="html5" />
                 <SkillBlock value="CSS 3" skill="css3" />
-                <SkillBlock value="Python" skill="python" />
-                <SkillBlock value="C++" skill="cplusplus" />
-                <SkillBlock value="Java" skill="java" />
             </div>
         </div>
     )
