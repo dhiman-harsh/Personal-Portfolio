99a63286074036f84688efc0b3dee6625845f7ef
Harsh Dhiman <harsh61355@gmail.com>
2026-04-01 18:27:54 +0530

    Updated Skill section: removed extra padding in desktop mode

--- src/Sections/Skills.jsx
+++ src/Sections/Skills.jsx
@@ -7,7 +7,7 @@ export const Skills = () => {
         <div className="px-4 md:px-8 flex flex-col" id="skills">
             <h1 className="text-semibold text-2xl">Skills</h1>
             {/* <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 my-10 px-10"> */}
-            <div className="md:px-12 flex flex-col gap-4 my-4">
+            <div className="flex flex-col gap-4 my-4">
                 <LibraryFramework />
                 <Languages />
                 <Tools />
