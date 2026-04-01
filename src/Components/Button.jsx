780de0bf2e08abff8a26aa908160821266ee814a
Harsh Dhiman <harsh61355@gmail.com>
2026-04-01 19:06:37 +0530

    Removed Social media icons border | changed pointer in Button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ value, link = "" }) => {
     return (
-        <button className="py-2 px-4 rounded-sm bg-blue-700 text-white active:scale-95 pointer md:w-fit">{value}</button>
+        <button className="py-2 px-4 rounded-sm bg-blue-700 text-white active:scale-95 md:w-fit pointer">{value}</button>
     )
 }
 
--- src/Components/SocialMediaIcon.jsx
+++ src/Components/SocialMediaIcon.jsx
@@ -1,6 +1,6 @@
 const SocialMediaIcon = ({img, url}) => {
     return (
-        <a href={url} target="_blank" className="border border-neutral-600 rounded-sm bg-[#222]">
+        <a href={url} target="_blank" className="rounded-sm bg-[#222]">
             <img src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${img.toLowerCase()}/${img.toLowerCase()}-original.svg`} alt={img} className="h-6 w-6" />
         </a>
     )
