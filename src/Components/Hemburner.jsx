34dedb016fb03cba248eb63069f4bf999accc0fa
Harsh Dhiman <harsh61355@gmail.com>
2026-04-01 23:36:09 +0530

    Hemburner menu icon updated

--- src/Components/Hemburner.jsx
+++ src/Components/Hemburner.jsx
@@ -5,12 +5,9 @@ const Hemburner = ({ menuOpen, setMenuOpen }) => {
         setMenuOpen(!menuOpen)
     }
     return (
-        <div className="md:hidden" onClick={()=>{menuHandler()}}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                fill="#e3e3e3">
-                <path
-                    d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
-            </svg>
+        <div className="md:hidden" onClick={() => { menuHandler() }}>
+            {menuOpen ? <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg> :
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>}
         </div>
     )
 }
