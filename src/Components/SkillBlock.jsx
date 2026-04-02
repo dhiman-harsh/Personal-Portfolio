30e1e0061d2d4ba40e3a5db00775df6a893c73fb
Harsh Dhiman <harsh61355@gmail.com>
2026-04-02 16:58:47 +0530

    Added border to skillBlock

--- src/Components/SkillBlock.jsx
+++ src/Components/SkillBlock.jsx
@@ -1,6 +1,6 @@
 const SkillBlock = ({value, skill}) => {
     return (
-        <div className="bg-[#222] flex flex-col gap-2 items-center rounded-sm py-2 px-4 h-40 justify-center">
+        <div className="bg-[#222] flex flex-col gap-2 items-center rounded-sm py-2 px-4 h-40 justify-center border border-neutral-600">
             <img src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${skill.toLowerCase()}/${skill.toLowerCase()}-original.svg`} className="h-16 w-16" />
             <span className="text-white">{value}</span>
         </div>
