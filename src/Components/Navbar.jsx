fde9e05ffff36c7e6e53a05a3f1699741832312d
Harsh Dhiman <harsh61355@gmail.com>
2026-04-02 17:28:56 +0530

    Adjusted Navbar Menu on Mobile changed its apperance

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,13 +7,13 @@ import { useState } from 'react'
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <div className="flex justify-between px-4 md:px-8 py-3 md:py-4 items-center sticky top-0 bg-[#222]">
+        <div className="flex justify-between px-4 md:px-8 py-3 md:py-4 items-center sticky top-0 bg-[#222] border-b border-neutral-600">
             <Logo />
             <Menu />
             <Hemburner menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             {
                 menuOpen ?
-                    <div className="absolute z-10 top-[100%] left-0 right-0 px-4 pb-4 flex justify-between bg-[#222]">
+                    <div className="absolute z-10 top-full left-0 right-0 py-4 justify-between flex flex-col gap-4 items-center border-y border-neutral-600 backdrop-blur-md bg-[#222]/[.8]">
                         <MenuButton value="Home" link="#home" />
                         <MenuButton value="Skills" link="#skills" />
                         <MenuButton value="Projects" link="#projects" />
