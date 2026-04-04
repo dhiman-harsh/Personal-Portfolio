c6c807c351059bf96f2bb4cdd6430d51bc241ccf
Harsh Dhiman <harsh61355@gmail.com>
2026-04-04 17:22:06 +0530

    Added padding top to Skill and Contact Section

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -6,7 +6,7 @@ import { ThemeContext } from "../Components/Context/ThemeContext"
 export const Contact = () => {
     const { theme } = useContext(ThemeContext)
     return (
-        <div className="flex flex-col px-4 md:px-8" id="contact">
+        <div className="flex flex-col px-4 md:px-8 " id="contact">
             <h1 className={`font-semibold text-2xl ${theme == 'light' ? 'text-[#111]' : 'text-[#eee]'}`}>Contact</h1>
             <ContactForm />
             <SocialMedia />
