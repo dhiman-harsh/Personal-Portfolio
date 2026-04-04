4e2b1e9f11cc517dcd5c40397aa5706c20182693
Harsh Dhiman <harsh61355@gmail.com>
2026-04-04 11:43:24 +0530

    Added new icons for social media links

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -3,8 +3,8 @@ import SocialMediaIcon from "./SocialMediaIcon"
 const SocialMedia = () => {
     return (
         <div className="flex gap-4 items-center justify-center mx-10 my-4 md:m-10">
-            <SocialMediaIcon img="linkedin" url="https://www.linkedin.com/in/dhiman-harsh" />
-            <SocialMediaIcon img="github" url="https://github.com/dhiman-harsh" />
+            <SocialMediaIcon path="https://s.magecdn.com/social/mb-github.svg" url="https://www.linkedin.com/in/dhiman-harsh" />
+            <SocialMediaIcon path="https://s.magecdn.com/social/mb-linkedin.svg" url="https://github.com/dhiman-harsh" />
         </div>
     )
 }
--- src/Components/SocialMediaIcon.jsx
+++ src/Components/SocialMediaIcon.jsx
@@ -1,7 +1,7 @@
-const SocialMediaIcon = ({img, url}) => {
+const SocialMediaIcon = ({path, url}) => {
     return (
-        <a href={url} target="_blank" className="rounded-sm bg-[#222]">
-            <img src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${img.toLowerCase()}/${img.toLowerCase()}-original.svg`} alt={img} className="h-6 w-6" />
+        <a href={url} target="_blank" className="rounded-sm bg-[#fff]">
+            <img src={path} className="h-6 w-6" />
         </a>
     )
 }
