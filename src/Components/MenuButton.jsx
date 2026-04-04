ba5e273fa7de51d0eef60bf3f226f086a9619f96
Harsh Dhiman <harsh61355@gmail.com>
2026-04-04 12:51:23 +0530

    Added color-white and scale-110 on hover on menu buttons

--- src/Components/MenuButton.jsx
+++ src/Components/MenuButton.jsx
@@ -1,6 +1,6 @@
 const MenuButton = ({ value, link }) => {
     return (
-        <a className="cursor-pointer" href={link}>{value}</a>
+        <a className="cursor-pointer text-neutral-200 hover:text-white hover:scale-110" href={link}>{value}</a>
 
     )
 }
