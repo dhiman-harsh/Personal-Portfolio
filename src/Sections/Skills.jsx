8ea3445c14e61ce20fb1237e5a9486a8c61b4dcd
Harsh Dhiman <harsh61355@gmail.com>
2026-04-04 17:28:33 +0530

    remved top padding

--- src/Sections/Skills.jsx
+++ src/Sections/Skills.jsx
@@ -7,7 +7,7 @@ import { ThemeContext } from "../Components/Context/ThemeContext"
 export const Skills = () => {
     const { theme } = useContext(ThemeContext)
     return (
-        <div className="px-4 md:px-8 flex flex-col pt-12" id="skills">
+        <div className="px-4 md:px-8 flex flex-col" id="skills">
             <h1 className={`font-semibold text-2xl ${theme == 'light' ? 'text-[#111]' : 'text-[#eee]'}`}>Skills</h1>
             {/* <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 my-10 px-10"> */}
             <div className="flex flex-col gap-4 my-4">
