112c8e170a78d863141a073901fc59f76ab5574c
Harsh Dhiman <harsh61355@gmail.com>
2026-04-05 17:55:22 +0530

    updated cursor height

--- src/Components/Role.jsx
+++ src/Components/Role.jsx
@@ -7,7 +7,7 @@ const Role = () => {
     return (
         <div className={`text-3xl md:text-4xl font-semibold my-1 min-h-11 md:min-w-96 ${theme == 'light' ? 'text-[#059669]' : 'text-[#10B981]'}`}>
             <MultiTypewriter />
-            <span className={`w-2 h-[1em] border-r-2 animate-pulse ${theme == 'light' ? 'border-[059669]' : 'border-[10B981]'}`}></span>
+            <span className={`w-2 h-xl border-r-2 animate-pulse ${theme == 'light' ? 'border-[059669]' : 'border-[10B981]'}`}></span>
         </div>
     )
 }
