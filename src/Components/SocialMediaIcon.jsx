aef399061cee2115afbdd8bef13be3b8fbbcd200
Harsh Dhiman <harsh61355@gmail.com>
2026-04-08 16:38:45 +0530

    maked social media icons background white

--- src/Components/SocialMediaIcon.jsx
+++ src/Components/SocialMediaIcon.jsx
@@ -1,6 +1,6 @@
 const SocialMediaIcon = ({path, url}) => {
     return (
-        <a href={url} target="_blank" className="rounded-sm overflow-hidden h-6 w-6">
+        <a href={url} target="_blank" className="rounded-md overflow-hidden h-6 w-6 bg-[#fff]">
             <img src={path} className="object-cover" />
         </a>
     )
