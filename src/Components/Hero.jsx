c79e56c50fe6b05074810297a52bd4d0e3e06d7f
Harsh Dhiman <harsh61355@gmail.com>
2026-04-08 15:42:05 +0530

    Added Profile Picture from linkedin using unavatar.io to fetch Linkedin profile picture

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     return (
         <div className="flex-1 flex flex-col items-center justify-center gap-6 md:gap-10 my-6 md:my-20">
             <div className="top flex items-start flex-col gap-6 md:flex-row md:gap-12">
-                <div className={`size-60 rounded-sm mx-auto shadow-lg ${theme == 'light' ? 'border-4 border-[#FFFFFF]' : 'border border-neutral-600'}`}></div>
+                <div className={`size-60 rounded-md mx-auto shadow-lg ${theme == 'light' ? 'border-4 border-[#FFFFFF]' : 'border border-neutral-600'} profile-picture`}></div>
                 <div className="px-4 md:px-0">
                     <div className={`type text-lg md:text-xl font-medium ${theme == 'light' ? 'text-[#475569]' : 'text-[#A3A3A3]'}`}>Hello, I am</div>
                     <div className={`type text-2xl md:text-3xl font-medium ${theme == 'light' ? 'text-[#0F172A]' : 'text-[#F5F5F5]'}`}>Harsh Dhiman</div>
